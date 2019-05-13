f679297620a7a279c01485fec5506670090b799e
hello_chenchen <welcomechenchen@gmail.com>
2019-05-13 22:08:19 +0800

    optimize: code optimize

--- inc/util/util.h
+++ inc/util/util.h
@@ -4,8 +4,8 @@
  * Copyright (c) 2019 cc-algorithm contributors:
  *   - hello_chenchen <https://github.com/hello-chenchen>
  *
- * MIT License <https://github.com/hello-chenchen/cclib-common/blob/master/LICENSE>
- * See https://github.com/hello-chenchen/cclib-common for the latest update to this file
+ * MIT License <https://github.com/cc-libraries/cclib-common/blob/master/LICENSE>
+ * See https://github.com/cc-libraries/cclib-common for the latest update to this file
  *
  * author: hello_chenchen <https://github.com/hello-chenchen>
  **********************************************************************************/
